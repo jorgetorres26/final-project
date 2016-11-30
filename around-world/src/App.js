8fa4e66611e744be64020c7b0acd5113dd275f6c
muhaamed <mold81193@gmail.com>
2016-11-29 18:03:53 -0800

    Basic footer added

--- around-world/src/App.js
+++ around-world/src/App.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Nav, NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
+import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div>
-        <Header/>
-        <NavBar/>
-        <Map/>
+        <Header />
+        <NavBar />
+        <main className="container" >
+          {this.props.children}
+        </main>
+        <Footer />
       </div>
     );
   }
 }
 
-class Header extends React.Component{
+class Header extends React.Component {
   render() {
     return (
-        <div>
-          <h2>Around the World</h2>
-        </div> 
+      <div>
+        <h2>Around the World</h2>
+      </div>
     );
   }
 }
 
-class NavBar extends React.Component{
+class NavBar extends React.Component {
 
   render() {
     return (
@@ -46,11 +52,62 @@ class NavBar extends React.Component{
   }
 }
 
-class Map extends React.Component{
+class Footer extends React.Component {
   render() {
     return (
-      <div id = "mapPic">
-        
+      <div className="container">
+
+        <div className="row">
+          <h3 className="footertext">About Us:</h3>
+          <br />
+          <div className="col-md-3">
+            <center>
+              <img src="http://avatarbox.net/avatars/img29/mr_burns_sneaky_avatar_picture_14912.gif" className="img-circle" alt="the-brains" />
+              <br />
+              <h4 className="footertext">Sarah<p className="text-muted">Developer</p></h4>
+              <p className="footertext">You can thank all the crazy programming here to this guy.</p><br />
+            </center>
+          </div>
+
+          <div className="col-md-3">
+            <center>
+              <img src="http://www.phpbbguru.net/community/download/file.php?avatar=43539_1384745414.jpg" className="img-circle" alt="..." />
+              <br />
+              <h4 className="footertext">Jorge<p className="text-muted">Developer</p></h4>
+              <p className="footertext">All the images here are hand drawn by this man.</p><br />
+            </center>
+          </div>
+
+          <div className="col-md-3">
+            <center>
+              <img src="http://avatarbox.net/avatars/img19/homer_jamaica_avatar_picture_18399.gif" className="img-circle" alt="..." />
+              <br />
+              <h4 className="footertext">Joe<p className="text-muted">Developer</p></h4>
+              <p className="footertext">This pretty site and the copy it holds are all thanks to this guy.</p><br />
+            </center>
+          </div>
+
+          <div className="col-md-3">
+            <center>
+              <img src="http://www.animated-gifs.eu/category_cartoons/avatars-100x100-cartoons-spongebob/0038.gif" className="img-circle" alt="the-brains" />
+              <br />
+              <h4 className="footertext">Muhaamed<p className="text-muted">Developer</p></h4>
+              <p className="footertext">You can thank all the crazy programming here to this guy.</p><br />
+            </center>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div id="mapPic">
+
       </div>
     )
   }
