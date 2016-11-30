2a16b8686f40edf9352b083bad3ea22c8931e26a
muhaamed <mold81193@gmail.com>
2016-11-29 19:01:24 -0800

    Added the routes to each page

--- around-world/src/App.js
+++ around-world/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage';
 import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Link, hashHistory } from 'react-router';
 
 class App extends React.Component {
   constructor(props) {
@@ -37,14 +39,17 @@ class NavBar extends React.Component {
     return (
       <nav className="navbar navbar-light bg-faded">
         <ul className="nav navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Explore <span className="sr-only">(current)</span></a>
+          <li className="nav-item">
+            <Link to="/explore" activeClassName="activeLink" className="nav-link">Explore</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Adventure</a>
+            <Link to="/adventure" activeClassName="activeLink" className="nav-link">Adventure</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Discover</a>
+            <Link to="/discover" activeClassName="activeLink" className="nav-link">Discover</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/home" activeClassName="activeLink" className="nav-link">Home</Link>
           </li>
         </ul>
       </nav>
--- around-world/src/index.js
+++ around-world/src/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Homepage from './HomePage';
+import Explore from './Explore';
+import Adventure from './Adventure';
+import Discover from './Discover';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
+//<IndexRoute component={Homepage} />
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    <Route path="home" component={Homepage}/>
+    <Route path="explore" component={Explore}/>
+    <Route path="adventure" component={Adventure}/>
+    <Route path="discover" component={Discover}/>
+    </Route>
+    
+    
+      
+  </Router>,
+  
   document.getElementById('root')
 );
