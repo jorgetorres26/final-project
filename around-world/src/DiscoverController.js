7e51dbb36a6dbd040d3be365915b225cf166ba67
jorgetorres26 <jorgetor1126@gmail.com>
2016-11-30 16:44:56 -0800

    Added interests and started countries API

--- around-world/src/DiscoverController.js
+++ around-world/src/DiscoverController.js
@@ -9,3 +9,12 @@ var newArray = _.shuffle(array);
 console.log(newArray);
 
 
+var interests = ['Arts', 'Beaches', 'Biking', 'Birding', 
+                'Business', 'Camping', 'Casinos', 'Cycling', 'Diving', 
+                'Golf', 'Hiking', 'Parks', 'Nightlife', 'Shopping', 
+                'Skiing', 'Surfing', 'Kayaking', 'Racing', 'Running', 
+                'Sky+Diving', 'Restaurants', 'Amusement+parks', 'Fishing',
+                'Bowling', 'Horseriding']
+
+
+var discoverEndpoint = 'https://api.theprintful.com/countries';
