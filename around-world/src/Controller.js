6a487481dacc5ed45cac3e81ad30ed02f0ba31f3
Joe Motto <joermotto@gmail.com>
2016-12-04 16:08:46 -0800

    wored on map

--- around-world/src/Controller.js
+++ around-world/src/Controller.js
@@ -1,7 +1,28 @@
+import 'whatwg-fetch'; //for polyfill
 
 
 // Google Places API
 
 var key = "AIzaSyDPqNFY-8y3iThJr9cy5GxZO18Qo7iCmmQ";
+var baseUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
 
-var keyIntro = "&key=";
+//module with functions to download a model from online
+var controller = {
+  searchTMDB: function(query) {
+    //var resource = '&part=snippet'
+    var tmdbUri = baseUrl+query+'&key='+key;
+    console.log(tmdbUri);
+    //console.log(fetch('https://www.googleapis.com/youtube/v3/videos?id=As2_UloSKDw&key=AIzaSyA-6GHSwcPBEmaZJQ2vSXaUzKPH7YJqvNU&part=snippet,statistics'));
+    return fetch(tmdbUri)
+      .then(function(res) {
+        res.set({
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+                'Access-Control-Allow-Methods':'POST, GET, OPTIONS',
+                'Access-Control-Allow-Headers':'*',
+                'Access-Control-Allow-Credentials': true});
+        return res.json()
+      })
+  }
+}
+
+export default controller;
--- around-world/src/Explore.js
+++ around-world/src/Explore.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link, hashHistory } from 'react-router';
+import {Panel, Form, FormControl, InputGroup, Button, Glyphicon, Image} from 'react-bootstrap';
 import './Explore.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-
+import Controller from './Controller';
 
 
 class Explore extends React.Component {
@@ -11,16 +12,34 @@ class Explore extends React.Component {
     this.state = {
       lat: 47.655548,
       lng: -122.303200,
-      zoom: 13,
+      places:[],
+      name:'',
+      zoom: 4
     };
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData(searchTerm) {
+    var thisComponent = this;
+    Controller.searchTMDB(searchTerm)
+      .then(function(data) {
+        
+        thisComponent.setState({places:data.results});
+        console.log(data);
+        ;
+      })
+      .catch( (err) => this.setState({places:[]}));
+        
   }
   render() {
     var position = [this.state.lat, this.state.lng];
+    {console.log(this.state)}
     return (
       <div >
       <h2>This is the Explore page</h2>
+      <SearchForm searchFunction={this.fetchData} />
         <div id="mapid">
-          <Map style={{height: "180px"}} center={position} zoom={this.state.zoom}>
+          <Map style={{height: "580px"}} center={position} zoom={this.state.zoom}>
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
@@ -33,6 +52,40 @@ class Explore extends React.Component {
           </Map>
         </div>
       </div>
+    );
+  }
+}
+
+class SearchForm extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {value:''}
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({value:event.target.value});
+  }
+
+  handleClick(event) {
+    this.props.searchFunction(this.state.value);
+  }
+
+  render() {
+    return (
+      <Form>
+        <InputGroup>
+          <InputGroup.Button>
+            <Button onClick={this.handleClick}>#</Button>
+          </InputGroup.Button>
+          <FormControl type="text" placeholder="Search for a video..." onChange={this.handleChange} />
+          <InputGroup.Addon>
+            {} results
+          </InputGroup.Addon>
+        </InputGroup>
+      </Form>
     );
   }
 }
