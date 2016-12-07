4115c49b97e0286f04c4df3b45263d7a0960798e
Sarah Phillips <sarahp39@uw.edu>
2016-12-06 21:10:45 -0800

    Working version of the quiz and better layout for the results

--- around-world/src/Adventure.js
+++ around-world/src/Adventure.js
@@ -13,85 +13,79 @@ class Adventure extends React.Component {
     super(props);
     this.state = {
       quizStarted: false,
-      result: {photo:'', info:''}
-    } 
+      result: { photo: '', info: '' }
+    }
     this.quizStart = this.quizStart.bind(this);
     this.fetchResult = this.fetchResult.bind(this);
   }
   //will change the status of the quizStarted when the start button is clicked
   quizStart() {
-    this.setState({quizStarted: true}); 
+    this.setState({ quizStarted: true });
   }
-// Show start button for quiz. Once clicked, quiz starts and start button is hidden
+  // Show start button for quiz. Once clicked, quiz starts and start button is hidden
 
-  fetchResult(string){
+  fetchResult(string) {
     var thisComponent = this;
     console.log(string);
     Controller.searchMap(string)
-      .then(function(data){
-        
+      .then(function (data) {
+
         var baseUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
         var key = "&key=AIzaSyAWOXIjZamr-SLhYSnzbMm4_VGbQ9EBmPI";
         var ref = data.results[0].photos[0].photo_reference;
         console.log(ref);
         thisComponent.setState({
-          result:{
-            photo:baseUrl+ref+key,
-            info:data.results[0]
+          result: {
+            photo: baseUrl + ref + key,
+            info: data.results[0]
           }
-        }); 
-    }).catch( (err) => this.setState({result:[]}));
+        });
+      }).catch((err) => this.setState({ result: [] }));
 
   }
   render() {
-     console.log(this.state);
-    
-    if(this.state.quizStarted == false){
-    return (
-      <div >
-        <h2>Take the Adventure Quiz to see where the perfect destination for you might be!!</h2>
-        <div id="contentContainer">
-          <Button className="startButton" bsStyle="primary" bsSize="large" block onClick={this.quizStart}>Start the Quiz</Button>
-        </div>
-      </div>
-    );
-  }
-  else{
-    return(
-      <div>
-        <h2> Adventure </h2>
-        <div id="contentContainer">
-        <Quiz resultFunction={this.fetchResult} />
-        {this.state.result.photo ?
-          <div className = "resultsInfo"> 
-            <h1> works </h1>
-            <img className="image" src={this.state.result.photo} />
-            <ul>
-              <li>
-                <p>Name: {this.state.result.info.name}</p>
-              </li>
-              <li>
-                <p>Address: {this.state.result.info.formatted_address}</p>
-              </li>
-            </ul>
+    console.log(this.state);
+
+    if (this.state.quizStarted == false) {
+      return (
+        <div >
+          <h2>Take the Adventure Quiz to see where the perfect destination for you might be!!</h2>
+          <div id="contentContainer">
+            <Button className="startButton" bsStyle="primary" bsSize="large" block onClick={this.quizStart}>Start the Quiz</Button>
           </div>
-          : null
-        }
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div>
+          <h2> Adventure Quiz </h2>
+          <div id="contentContainer">
+            <Quiz resultFunction={this.fetchResult} />
+            {this.state.result.photo ?
+              <div className="resultsInfo">
+                <h3> Try this place! </h3>
+                <img className="image" src={this.state.result.photo} />
+                <p>Name: {this.state.result.info.name}</p>
+                <p>Address: {this.state.result.info.formatted_address}</p>
+              </div>
+              : null
+            }
+          </div>
+        </div>
+      );
+    }
   }
-  }
-  
+
 }
 
 // The quiz pages - Sarah
 class Quiz extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
-      answerNum:0,
-      answerString:''
+    this.state = {
+      answerNum: 0,
+      answerString: ''
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleResult = this.handleResult.bind(this);
@@ -99,98 +93,98 @@ class Quiz extends React.Component {
   handleClick(answer) {
     var i = this.state.answerNum;
     i++;
-    this.setState({answerNum: i});
-    this.setState({answerString: this.state.answerString +' '+answer});
+    this.setState({ answerNum: i });
+    this.setState({ answerString: this.state.answerString + ' ' + answer });
     console.log(this.state);
   }
 
   handleResult(event) {
-    this.props.resultFunction(this.state.answerArray);
+    this.props.resultFunction(this.state.answerString);
     console.log(this.state.answerString);
   }
 
   render() {
     var page = this.state.answerNum;
-    switch(page){
+    switch (page) {
       case 0: return (
-      <div id="quizContainer">
-        <Image className = "image"src = "http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/166142726.jpg" rounded></Image>
-        <p className="questionText">What is your budget?</p>
-        <div className="answers">
-          <Button className="answerButton" onClick={(e) => { this.handleClick('affordable') }}>$ Almost Free</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('cheap')}}>$$ Little Money</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('expensive')}}>$$$ Some Money</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('') }}>$$$$ No Budget</Button>
+        <div id="quizContainer">
+          <Image className="image" src="http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/166142726.jpg" rounded></Image>
+          <p className="questionText">What is your budget?</p>
+          <div className="answers">
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>$ Almost Free</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>$$ Little Money</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>$$$ Some Money</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>$$$$ No Budget</Button>
           </div>
-      </div>
-    );
-    case 1:  return (
-      <div id="quizContainer">
-        <Image className = "image"src = "http://www.worldatlas.com/aatlas/newart/continentslg.gif" rounded></Image>
-        <p className="questionText">Where would you prefer to explore?</p>
-        <div className="answers">
-          <Button className="answerButton" onClick={(e) => { this.handleClick('america') }}>North or South America</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('') }}>AnyWhere</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('europe') }}>Europe</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('asia') }}>Asia</Button>
+        </div>
+      );
+      case 1: return (
+        <div id="quizContainer">
+          <Image className="image" src="http://www.worldatlas.com/aatlas/newart/continentslg.gif" rounded></Image>
+          <p className="questionText">Where would you prefer to explore?</p>
+          <div className="answers">
+            <Button className="answerButton" onClick={(e) => { this.handleClick('america') } }>North America</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>No Preference</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('europe') } }>Europe</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('asia') } }>Asia</Button>
           </div>
-      </div>
-    );
-    case 2:   return (
-      <div id="quizContainer">
-        <Image className = "image"src = "http://www.familysmart.net/images/posts/family-travel-tips.jpg" rounded></Image>
-        <p className="questionText">Who are you traveling with?</p>
-        <div className="answers">
-          <Button className="answerButton" onClick={(e) => { this.handleClick('Singles') }}>Me, Myself, and I</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('Romantic') }}>My Lover</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('Family') }}>My Family</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('Group') }}>My Friends</Button>
+        </div>
+      );
+      case 2: return (
+        <div id="quizContainer">
+          <Image className="image" src="http://www.familysmart.net/images/posts/family-travel-tips.jpg" rounded></Image>
+          <p className="questionText">Who are you traveling with?</p>
+          <div className="answers">
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>Me, Myself, and I</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('Romantic') } }>My Lover</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('Family') } }>My Family</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>My Friends</Button>
           </div>
-      </div>
-    );
-    case 3:   return (
-      <div id="quizContainer">
-        <Image className = "image"src = "http://wdy.h-cdn.co/assets/cm/15/09/54f0fbd48fba0_-_1-couple-vacation-tropical-lgn.jpg" rounded></Image>
-        <p className="questionText">Why are you traveling?</p>
-        <div className="answers">
-          <Button className="answerButton" onClick={(e) => { this.handleClick('club') }}>I Want to Celebrate</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('Beach') }}>I Want to Escape and Relax</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('hiking') }}>I Want to Adventure and Explore</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('Museum') }}>I want to Learn Something New</Button>
+        </div>
+      );
+      case 3: return (
+        <div id="quizContainer">
+          <Image className="image" src="http://wdy.h-cdn.co/assets/cm/15/09/54f0fbd48fba0_-_1-couple-vacation-tropical-lgn.jpg" rounded></Image>
+          <p className="questionText">Why are you traveling?</p>
+          <div className="answers">
+            <Button className="answerButton" onClick={(e) => { this.handleClick('club') } }>I Want to Celebrate</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('beach') } }>I Want to Escape and Relax</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('hiking') } }>I Want to Adventure and Explore</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('museum') } }>I want to Learn Something New</Button>
           </div>
-      </div>
-    );
-    case 4:   return (
-      <div id="quizContainer">
-        <Image className = "image"src = "https://www.visitnc.com/resimg.php/imgcrop/2/38377/preview/800/480/HikeWaterfall.jpg" rounded></Image>
-        <p className="questionText">How Close Would You Like to be to Nature?</p>
-        <div className="answers">
-          <Button className="answerButton" onClick={(e) => { this.handleClick('park') }}>Super Close</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('waterfall') }}>Semi Close</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('lakes') }}>Not So Close</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('') }}>Nature? No thanks!</Button>
+        </div>
+      );
+      case 4: return (
+        <div id="quizContainer">
+          <Image className="image" src="https://www.visitnc.com/resimg.php/imgcrop/2/38377/preview/800/480/HikeWaterfall.jpg" rounded></Image>
+          <p className="questionText">How Close Would You Like to be to Nature?</p>
+          <div className="answers">
+            <Button className="answerButton" onClick={(e) => { this.handleClick('park') } }>Super Close</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('waterfall') } }>Semi Close</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('lakes') } }>Not So Close</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>Nature? No thanks!</Button>
           </div>
-      </div>
-    );
-    case 5:   return (
-      <div id="quizContainer">
-        <Image className = "image"src = "http://images.r.cruisecritic.com/features/2016/03/10-lux-cruise-main.jpg" rounded></Image>
-        <p className="questionText">How would you like to travel?</p>
-        <div className="answers" onClick={this.fetchData}>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('') }}>Car or Bus</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('cruise') }}>Boat</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('') }}>Plane</Button>
-          <Button className="answerButton" onClick={(e) => { this.handleClick('') }}>No Preference</Button>
+        </div>
+      );
+      case 5: return (
+        <div id="quizContainer">
+          <Image className="image" src="http://images.r.cruisecritic.com/features/2016/03/10-lux-cruise-main.jpg" rounded></Image>
+          <p className="questionText">How would you like to travel?</p>
+          <div className="answers" onClick={this.fetchData}>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>Car or Bus</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('cruise') } }>Boat</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>Plane</Button>
+            <Button className="answerButton" onClick={(e) => { this.handleClick('') } }>No Preference</Button>
           </div>
-      </div>
-    );
-    case 6: 
-      return (
-      <div id="quizContainer">
-        <h3> RESULTS: </h3>
-        <Button onClick={this.handleResult}>Show Results</Button>
-      </div>
-    );
+        </div>
+      );
+      case 6:
+        return (
+          <div id="quizContainer">
+            <h3> RESULTS: </h3>
+            <Button onClick={this.handleResult}>Show Results</Button>
+          </div>
+        );
     }
 
   }
